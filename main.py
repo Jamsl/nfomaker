1d48ddec568c4c27eb447910eb1c55c7a3e1d7b1
James Charnock <james.charnock@gmail.com>
2014-02-07 10:53:06 +0000

    V4

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 __author__ = 'James Charnock'
 
 from tkinter import *
-from tkinter import ttk
-import sys, os
+# from tkinter import ttk
+import sys
+import os
+from tkinter import filedialog
+
 
 entries = []
 checkboxes = []
 tags = []
+
 
 def get_list_of_files():
     pathname = os.path.dirname(sys.argv[0])
@@ -16,17 +20,18 @@ def get_list_of_files():
             l.append(file)
     return l
 
+
 def choose_XML():
     print(radio_file.get())
-    make_Tags_List(radio_file.get())
+    make_tags_list(radio_file.get())
     print(tags)
-    make_Grid(tags)
-
+    make_grid(tags)
     # grey out radio choices
-    for i in radio_array:
-        i.config(state=DISABLED)
+    # for i in radio_array:
+    #     i.config(state=DISABLED)
 
-def make_Tags_List(f):
+
+def make_tags_list(f):
     global tags
     tags = []
     with open(f) as inputfile:
@@ -34,19 +39,26 @@ def make_Tags_List(f):
             tags.append(line.strip())
     return tags
 
-def make_Grid(t):
+
+def make_grid(t):
     global entries
     global checkboxes
+    global frame_tags
     entries = []
     checkboxes = []
-# create labels, text entry boxes, checkboxes from tags
 
+# new 04-feb-14
+    frame_tags.destroy()
+    frame_tags = Frame(master)
+    frame_tags.grid(row=1)
+
+# create labels, text entry boxes, checkboxes from tags
     for i, a in enumerate(t):
-        Label(frame_tags, text=a.title(), width=10, anchor=W).grid(row = i+len(list_of_files), column=0, sticky=W)
+        Label(frame_tags, text=a.title(), width=10, anchor=W).grid(row=i+len(list_of_files), column=0, sticky=W)
         e = Entry(frame_tags, width=50)
         e.grid(row=i+len(list_of_files), column=1, sticky=W)
         entries.append(e)
-        entries[i].insert(INSERT, 'Enter '+ a)
+        entries[i].insert(INSERT, 'Enter ' + a)
         v = IntVar()
         c = Checkbutton(frame_tags, variable=v)
         c.grid(row=i+len(list_of_files), column=2, sticky=W)
@@ -54,12 +66,13 @@ def make_Grid(t):
         checkboxes.append(c)
         checkboxes[i].toggle()
         if a == 'episode':
-            up_episodes_button = Button(frame_tags, text="^", command = increment_episodes)
-            up_episodes_button.grid(row=i+len(list_of_files), column = 5)
-            down_episodes_button = Button(frame_tags, text="v", command = decrement_episodes)
-            down_episodes_button.grid(row=i+len(list_of_files), column = 6)
+            up_episodes_button = Button(frame_tags, text="^", command=increment_episodes)
+            up_episodes_button.grid(row=i+len(list_of_files), column=5)
+            down_episodes_button = Button(frame_tags, text="v", command=decrement_episodes)
+            down_episodes_button.grid(row=i+len(list_of_files), column=6)
 
     return entries, checkboxes
+
 
 def increment_episodes():
     try:
@@ -69,6 +82,7 @@ def increment_episodes():
     except ValueError:
         entries[tags.index('episode')].delete(0, END)
         entries[tags.index('episode')].insert(0, 1)
+
 
 def decrement_episodes():
     try:
@@ -80,10 +94,10 @@ def decrement_episodes():
         entries[tags.index('episode')].insert(0, 1)
 
 
-
 # create XML tag
 def make_XML_Tag(tag, value):
     return '    <' + str(tag) + '>' + str(value) + '</' + str(tag) + '>'
+
 
 def make_NFO():
 #     print('<movie>')
@@ -106,6 +120,11 @@ def create_NFO_file():
     f = open(filename_entry.get() + '.nfo', 'w')
     f.write(make_nfo_str())
 
+
+def save_as_NFO_file():
+    f = open(filedialog.asksaveasfilename(defaultextension="nfo", initialfile=filename_entry.get()), 'w')
+    f.write(make_nfo_str())
+
 def make_nfo_str():
     nfo = '<' + radio_file.get()[:-4] + '>\n'
     for i, a in enumerate(entries):
@@ -116,6 +135,7 @@ def make_nfo_str():
             nfo += '\n'
     nfo += '</' + radio_file.get()[:-4] + '>'
     return nfo
+
 
 def make_filename():
     if radio_file.get() == 'episodedetails.txt':
@@ -134,13 +154,13 @@ master = Tk()
 master.title("XBMC NFO generator")
 
 frame_header = Frame(master)
-frame_header.grid(sticky=W)
+frame_header.grid(sticky=W, row=0)
 
 frame_tags = Frame(master)
-frame_tags.grid()
+frame_tags.grid(row=1)
 
 frame_footer = Frame(master)
-frame_footer.grid()
+frame_footer.grid(row=2)
 
 print(list_of_files)
 
@@ -158,11 +178,14 @@ radio_file.set(list_of_files[0])
 #     i.deselect()
 
 
-generate_button = Button(frame_footer, text="Generate NFO", command=make_NFO)
+generate_button = Button(frame_footer, text="Show XML", command=make_NFO)
 generate_button.grid()
 
-generate_button = Button(frame_footer, text="Create NFO file", command=create_NFO_file)
-generate_button.grid()
+create_nfo_button = Button(frame_footer, text="Create NFO file", command=create_NFO_file)
+create_nfo_button.grid()
+
+save_as_nfo_button = Button(frame_footer, text="Save as...", command=save_as_NFO_file)
+save_as_nfo_button.grid(row=create_nfo_button.grid_info()["row"], column=1)
 
 
 series_entry = Entry(frame_footer, width=70)
@@ -170,15 +193,16 @@ series_entry.grid()
 series_entry.insert(INSERT, 'Enter series name')
 
 
-
 filename_entry = Entry(frame_footer, width=80)
 filename_entry.grid()
 
+nfo_textbox_scrollbar = Scrollbar(frame_footer)
+nfo_textbox_scrollbar.grid(column=1, sticky=N+S)
 
+nfo_textbox = Text(frame_footer, width=70, height=10, yscrollcommand=nfo_textbox_scrollbar.set)
+nfo_textbox.grid(row=nfo_textbox_scrollbar.grid_info()["row"])
 
-nfo_textbox = Text(frame_footer, width=70)
-nfo_textbox.grid()
-
+nfo_textbox_scrollbar.config(command=nfo_textbox.yview)
 
 
 print(radio_file.get())
